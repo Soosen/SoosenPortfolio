7f7f75640d079a27da488e582ab43f561c866798
Soosen <mateusz.sosnowski.zg@gmail.com>
2024-05-17 21:15:35 +0200

    HomePage final touches

--- src/components/TopPageMainSection.jsx
+++ src/components/TopPageMainSection.jsx
@@ -1,19 +1,41 @@
 import GenericButton from "./GenericButton";
-import "./TopPageMainSecion.css";
-import React, { useState } from "react";
+import "./TopPageMainSeciton.css";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
-function TopPageMainSecion() {
+function TopPageMainSeciton() {
+  const [avatarRotation, setRotation] = useState(0); // State to manage rotation angle
+  const [avatarRotationSpeed, setRotationSpeed] = useState(1); // Speed of rotation in degrees per frame
   const navigate = useNavigate();
+
+  useEffect(() => {
+    // Animation loop
+    const updateRotation = () => {
+      setRotation((prevRotation) => (prevRotation + avatarRotationSpeed) % 360);
+      requestAnimationFrame(updateRotation);
+    };
+
+    requestAnimationFrame(updateRotation);
+
+    // No cleanup function needed
+  }, [avatarRotationSpeed]);
+
   return (
     <div className="FullPageView">
       <div className="MainSectionContainer">
         <div className="AvatarContainer">
-          <img
-            src="src/assets/avatar.png"
-            alt="avatar_picture"
-            className="Avatar"
-          />
+          <div
+            className="AvatarBorder"
+            style={{
+              background: `linear-gradient(${avatarRotation}deg,#8b00ff, #0022ff, #00c3ff, #00ff00)`,
+            }}
+          >
+            <img
+              src="src/assets/avatar.png"
+              alt="avatar_picture"
+              className="Avatar"
+            />
+          </div>
         </div>
         <div className="TitleContainer">
           <h1>Hi, I am Soosen, an aspiring junior software developer.</h1>
@@ -49,4 +71,4 @@ function TopPageMainSecion() {
   );
 }
 
-export default TopPageMainSecion;
+export default TopPageMainSeciton;
