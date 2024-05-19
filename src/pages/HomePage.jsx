6ba67e0a63f2e1087fe33c5fc3913bc39da68b4c
Soosen <mateusz.sosnowski.zg@gmail.com>
2024-05-20 00:32:34 +0200

    add mobile view in progress message

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,29 @@
+import React, { useEffect, useState } from "react";
 import "./HomePage.css";
 import NavBar from "../components/NavBar";
-import TopPageMainSecion from "../components/TopPageMainSection";
+import TopPageMainSection from "../components/TopPageMainSection";
 
 function HomePage() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const userAgent = navigator.userAgent;
+    const mobileDevices =
+      /Mobi|Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
+
+    setIsMobile(mobileDevices.test(userAgent));
+  }, []);
+
   return (
     <div className="Page">
-      <NavBar></NavBar>
-      <div className="MainSection">
-        <TopPageMainSecion></TopPageMainSecion>
-      </div>
+      <NavBar />
+      {isMobile ? (
+        <div className="MobileMessage">Mobile view in progress</div>
+      ) : (
+        <div className="MainSection">
+          <TopPageMainSection />
+        </div>
+      )}
     </div>
   );
 }
